cc054d8b55be3bd90b1e88aa00bc86865c7bd2ad
kor <kor.dwarshuis@gmail.com>
2013-10-23 00:49:57 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>verySmallAndBasicAudioPlayer</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no" />
+    <link href="css/general.css" rel="stylesheet">
+</head>
+<body>
+    <img id="play2"  src="img/Emotes-face-embarrassed-icon.png" alt="face embarrassed" />
+    <img id="play11" src="img/Emotes-face-uncertain-icon.png" alt="face uncertain" />
+    <img id="play3"  src="img/Emotes-face-kiss-icon.png" alt="face kiss" />
+    <img id="play4"  src="img/Emotes-face-laugh-icon.png" alt="face laugh" />
+    <img id="play5"  src="img/Emotes-face-plain-icon.png" alt="face plain" />
+    <img id="play6"  src="img/Emotes-face-raspberry-icon.png" alt="face raspberry" />
+    <img id="play7"  src="img/Emotes-face-sad-icon.png" alt="face sad" />
+    <img id="play8"  src="img/Emotes-face-smile-big-icon.png" alt="face smile big" />
+    <img id="play10" src="img/Emotes-face-surprise-icon.png" alt="face surprise" />
+    <img id="play12" src="img/Emotes-face-wink-icon.png" alt="face wink" />
+
+    <button class="minimal" id="start">start repetitive fart</button>
+    <button class="minimal" id="stop">stop repetitive fart</button>
+
+    <p>
+        sound credits:
+        <a href="http://freesound.org/people/Robinhood76/sounds/124738/">http://freesound.org/people/Robinhood76/sounds/124738/</a>
+        <a href="http://freesound.org/people/thecluegeek/sounds/140583/">http://freesound.org/people/thecluegeek/sounds/140583/</a>
+        <a href="http://freesound.org/people/mimikrycz/sounds/149492/">http://freesound.org/people/mimikrycz/sounds/149492/</a>
+        <a href="http://freesound.org/people/OwlStorm/sounds/151223/">http://freesound.org/people/OwlStorm/sounds/151223/</a>
+        <a href="http://freesound.org/people/esperar/sounds/170781/">http://freesound.org/people/esperar/sounds/170781/</a>
+        <a href="http://freesound.org/people/jorickhoofd/sounds/180298/">http://freesound.org/people/jorickhoofd/sounds/180298/</a>
+        <a href="http://freesound.org/people/TeamMasaka/sounds/197430/">http://freesound.org/people/TeamMasaka/sounds/197430/</a>
+        <a href="http://freesound.org/people/roscoetoon/sounds/26403/">http://freesound.org/people/roscoetoon/sounds/26403/</a>
+        <a href="http://freesound.org/people/bennychico11/sounds/29543/">http://freesound.org/people/bennychico11/sounds/29543/</a>
+        <a href="http://freesound.org/people/Robinhood76/sounds/61225/">http://freesound.org/people/Robinhood76/sounds/61225/</a>
+    </p>
+    <p>
+        icons credit: <a href="http://www.iconarchive.com/">http://www.iconarchive.com/</a>
+    </p>
+
+    <a href="http://www.woudziel.nl"><img id="logo" src="../img/logo.png" alt="dwarshuis front-end web developer"/></a>
+    <script src="js/general.js"></script>
+</body>
+</html>
